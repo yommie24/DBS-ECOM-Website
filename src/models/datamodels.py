40fd8e3e8059252129a897dc4eb627988271f9fa
2600 <78653311+CookieStudios5609@users.noreply.github.com>
2023-10-14 13:35:28 -0400

    Feat: Add JWT token models and fix improper optional parameter syntax.

--- src/models/datamodels.py
+++ src/models/datamodels.py
@@ -2,33 +2,50 @@ from pydantic import BaseModel
 import uuid
 
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: str
+    user_id: str
+
+
 class AccountNoId(BaseModel):
+    """A user account but without the ID. Used for parsing input and registering users"""
     name_f: str
-    name_l: str = None
+    name_l: str | None = None
     email: str
     password: str
-    address: str = None
-    contact: str = None
+    address: str | None = None
+    contact: str | None = None
 
 
 class Account(AccountNoId):
+    """A full model of the user account in the database."""
     acct_id: uuid.UUID
 
 
-
-
+class Customer(BaseModel):
+    """User data for the frontend."""
+    user_id: str
+    name_f: str
+    name_l: str | None = None
+    cart: str | None = None
+    prefs: str | None = None
 
 
 class ItemNoId(BaseModel):
     """A representation of a full item for sale in the database."""
-    # TODO: Come back and implement images/tags. SQLite doesn't support lists/arrays
+    # TODO: /items/new still has seller id in request body. Make a new model since seller id comes from token now.
     name: str
-    image: str = None
-    price: float = None
-    desc: str = None
-    tag: str = None
-    seller: uuid.UUID = None
-    sku: str = None
+    image: str | None = None
+    price: float | None = None
+    desc: str | None = None
+    tag: str | None = None
+    seller: uuid.UUID | None = None
+    sku: str | None = None
 
 
 class Item(ItemNoId):
@@ -42,6 +59,6 @@ class ListedItem(BaseModel):
     These only appear on the browse/search page. The full item is used for the item's own page."""
     item_id: int
     name: str
-    thumbnail: str = None
+    thumbnail: str | None = None
     price: float
-    tag: str = None
+    tag: str | None = None
