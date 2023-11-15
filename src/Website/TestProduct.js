0436e4bdd4f21647a616758e2c7b238cff00a3ce
yommie24 <yommie24@gmail.com>
2023-11-15 07:27:24 -0500

    Update TestProduct.js

--- src/Website/TestProduct.js
+++ src/Website/TestProduct.js
@@ -1,3 +1,56 @@
+document.onload = TestProduct();
+
+async function TestProduct() {
+    const item = await getItem();
+    displayItem(item);
+
+}
+async function getItem() {
+
+    // Storing response
+    const response = await fetch(window.location.origin + "/items/item/1");
+
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+    return item;
+}
+function buildItemChild(item) {
+    let product = document.createElement("div");
+    product.setAttribute("class", "product");
+    let name = document.createElement("h1");
+    name.innerText = item["name"]
+    //             <p>$250</p>
+    let price = document.createElement("h2");
+    price.innerText = "$" + item["price"]
+    desc.appendChild(name);
+    desc.appendChild(price);
+
+    return product;
+
+}
+
+function displayItem(item) {
+    console.log(item)
+    var mainBody = document.getElementById("product");
+    item.forEach(item => {
+        mainBody.appendChild(
+            buildItemChild(item)
+        );
+    });
+}
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
 function showSlides(n) {
     let i;
