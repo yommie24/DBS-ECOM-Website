ebccb0f71b39aaaa173579c0571cd139e2593b48
yommie24 <yommie24@gmail.com>
2023-11-14 22:28:09 -0500

    Add files via upload

---
+++ src/Website/TestProduct.js
@@ -0,0 +1,70 @@
+document.onload = TestProduct();
+
+async function TestProduct() {
+    const item = await getItem();
+    displayItem(item);
+
+}
+async function getItem() {
+
+    // Storing response
+    const response = await fetch(window.location.origin + "/items/item/1");
+
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+    if (response) {
+        hideloader();
+    }
+    show(data);
+}
+function buildItemChild(item) {
+    let product = document.createElement("div");
+    product.setAttribute("class", "product");
+    let name = document.createElement("h1");
+    name.innerText = item["name"]
+    //             <p>$250</p>
+    let price = document.createElement("h2");
+    price.innerText = "$" + item["price"]
+    desc.appendChild(name);
+    desc.appendChild(price);
+
+}
+
+function displayItem(item) {
+    console.log(item)
+    var mainBody = document.getElementById("product");
+    shoes.forEach(item => {
+        mainBody.appendChild(
+            buildItemChild(item)
+        );
+    });
+}
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
