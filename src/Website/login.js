901356628e91ffade65f52b6ddea46c13ce9c3ce
yommie24 <yommie24@gmail.com>
2023-11-15 12:38:58 -0500

    Update login.js

--- src/Website/login.js
+++ src/Website/login.js
@@ -3,20 +3,21 @@ async function loginUser() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
+  const formData = new FormData();
+  formData.append('username', username);
+  formData.append('password', password);
+
   const response = await fetch("http://127.0.0.1:8000/token", {
-    method: "POST", 
-    body: new FormData() {
-      append('username', username),
-      append('password', password)
-    }
+    method: "POST",
+    body: formData
   });
 
   const data = await response.text();
 
   if (data.includes('access_token')) {
     // successful login
-    console.log("Login successful!"); 
-  } else {
+    console.log("Login successful!");
+  } else {   
     // invalid credentials
     console.log("Invalid credentials");
   }
